c869fb64a82107256ce70eb558f61034ee6db10f
fayentss <fayents.clement@gmail.com>
2022-11-08 01:19:40 +0100

    exo lundi

--- exo.py
+++ exo.py
@@ -8,19 +8,21 @@ def multiply(x,y):
     return(x * y)
 
 def divide(x,y):
-    return(x / y)
+    if y == 0:
+        return("erreur")
+    else:
+        return(x / y)
 
 def modulo(x,y):
-    return(x % y)
+    if y == 0:
+        return("erreur")
+    else:
+        return(x % y)
 
 
-def salaireSeconde(heure, salaireHoraire, heureParJourOuvrable, nonJourOuvrable):
-        
+def salaireSeconde(salaireHoraire, heureParJourOuvrable, nonJourOuvrable):
+    salaire = (((7-nonJourOuvrable)*4)*heureParJourOuvrable)*salaireHoraire
+    salaireSec = salaireHoraire/3600
 
-
-
-add sub multiply divide modulo 
-
-#calcule du salaire par seconde en fonction du salaire( Heure, salaireHoraire, heureParJourOuvrable, nonJourOuvrable )
-
-#calcule du salaire net( salairebrut, coef )
+def salaireNet(salaireBrut, coef):
+    return(salaireBrut*(1-(coef/100)))
